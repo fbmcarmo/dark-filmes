7d0e2fcf15b26db3340a24bc64a90e198d66c145
fbmcarmo <fbrunomcarmo@gmail.com>
2025-05-09 11:58:15 -0300

    Novas alterações

--- src/components/CardFilme/index.jsx
+++ src/components/CardFilme/index.jsx
@@ -1,19 +1,24 @@
-export default function CardFilme(){
+export default function CardFilme({ filme }){
     return (
-        <div className="w-[310px] h-[240px] flex flex-col rounded-lg bg-[#222222]">
-            <div className="w-full h-[70%] rounded-t-lg">
+        <div className="w-[310px] h-[240px] flex flex-col rounded-lg bg-[#222222]
+         cursor-pointer border border-transparent hover:border hover:border-[#8F7BD8] transition-all duration-300 ease-in-out">
+            <div className="w-full h-[70%] rounded-t-lg relative">
                 <img 
                     className="w-full h-full rounded-t-lg object-cover"
-                    src="https://image.api.playstation.com/vulcan/ap/rnd/202206/0720/0kRqUeSBIbQzz7cen3c989c6.jpg"
-                    alt=""    
+                    src={filme.banner}
+                    alt={filme.titulo}   
                 />
+                <div className="absolute top-1 left-2 flex text-[#9ca3af]">
+                    <p className="text-[17px] text-green-400 font-semibold">{filme.nota}</p>
+                    <p>/10</p>
+                </div>
             </div>
-            <div className="w-full h-[30%] flex flex-col pt-1 px-4">
-                <p className="text-[15px] font-bold">The Last of Us</p>
-                <p className="text-[15px] text-[#8a898c]">Neil Druckmann</p>
+            <div className="w-full h-[30%] flex flex-col pt-0.5 px-4">
+                <p className="text-[15px] font-bold">{filme.titulo}</p>
+                <p className="text-[15px] text-[#8a898c]">{filme.diretor}</p>
                 <div className="w-full flex justify-between">
-                    <p className="text-[15px] text-[#8a898c]">2019</p>
-                    <div className="px-1 py-0 bg-[#8F7BD8]/10 text-[#8F7BD8] rounded-lg">Ação</div>
+                    <p className="text-[15px] text-[#8a898c]">{filme.ano}</p>
+                    <div className="px-1 py-0 bg-[#8F7BD8]/10 text-[#8F7BD8] rounded-lg">{filme.genero}</div>
                 </div>
             </div>
         </div>
