c0aee70ee1b37846491c0ccfc2ed05fa9114d209
fbmcarmo <fbrunomcarmo@gmail.com>
2025-05-12 12:07:55 -0300

    Novas alterações

--- src/components/CardFilme/index.jsx
+++ src/components/CardFilme/index.jsx
@@ -1,6 +1,16 @@
+import { useRouter } from "next/router"
+
 export default function CardFilme({ filme }){
+    const router = useRouter()
+
+    function handleClickNavigate(){
+        router.push(`/filme/${filme.id}`)
+    }
+
     return (
-        <div className="w-[310px] h-[240px] flex flex-col rounded-lg bg-[#222222]
+        <div 
+        onClick={handleClickNavigate}
+        className="w-[310px] h-[240px] flex flex-col rounded-lg bg-[#222222]
          cursor-pointer border border-transparent hover:border hover:border-[#8F7BD8] transition-all duration-300 ease-in-out">
             <div className="w-full h-[70%] rounded-t-lg relative">
                 <img 
--- src/pages/filme/[id].jsx
+++ src/pages/filme/[id].jsx
@@ -1,16 +1,34 @@
 import PageWrapper from "@/components/PageWrapper"
 import { useRouter } from "next/router"
 import { LuArrowLeft } from "react-icons/lu"
+import { PiNotePencil } from "react-icons/pi"
+import { FiTrash2 } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import instance from "@/api/instance";
 
 export default function Filme(){
     const router = useRouter()
     const { id } = router.query
 
+    const [filme, setFilme] = useState({})
+
+  useEffect(() => {
+    async function getFilmesById(){
+      const response = await instance.get(`/api/movies/${id}`)
+      setFilme(response.data)
+    }
+
+    if(id){
+        getFilmesById()
+    }
+
+  }, []);
+
     return (
         <PageWrapper 
             showHeader={false}
         >
-            <div className="w-full h-full flex flex-col px-[350px] pt-12 items-start">
+            <div className="w-full h-full min-h-sreen flex flex-col px-[350px] pt-12 items-start">
                 <button 
                     onClick={() => router.back()}
                     className="px-5 py-2 text-[#8a898c] gap-2 flex items-center
@@ -18,6 +36,52 @@ export default function Filme(){
                     <LuArrowLeft />
                     <p>Voltar para a lista de filmes</p>
                 </button>
+                <div className="w-full min-h-[90vh] flex">
+                    <div className="w-[40%] h-full flex flex-col">
+                        <div className="w-full h-[90%]">
+                            <img
+                                className="w-full h-full rounded-lg object-cover"
+                                src="https://cl.buscafs.com/www.tomatazos.com/public/uploads/images/2879/2879_800x1250.jpg"
+                                alt=""
+                            />
+                        </div>
+                        <div className="w-full flex h-[10%] pt-4 justify-between">
+                            <button className="w-[46%] h-full border border-[#9b87f533]
+                             rounded-md text-[#9b87f5] hover:bg-[#9b87f5]/20 flex items-center justify-center gap-2 py-2">
+                                <PiNotePencil size={20} /> Editar filme</button>
+                            <button className="w-[46%] h-full border border-[#ef444433]
+                             text-[#ef4444] hover:bg-[#ef4444]/20 rounded-md flex items-center justify-center gap-2 py-2">
+                              <FiTrash2 size={20} />  Excluir filme
+                            </button>
+                        </div>
+                    </div>
+                     <div className="w-[60%] pl-4 h-full flex flex-col">
+                        <div className="w-full flex gap-2 items-baseline">
+                            <h1 className="text-[35px] text-[#9B87F5] font-bold">Interstellar</h1>
+                            <p className="text-[#8a898c] font-semibold text-[17px]">(2014)</p>
+                        </div>
+                        <div className="w-full gap-4 flex">
+                            <div className="py-1 px-4 rounded-2xl bg-[#4ade80]/20 text-[#4ade80]">
+                                <p className="text-[17px]">10/10</p>
+                            </div>
+                            <div className="py-1 px-4 rounded-2xl bg-[#9b87f5]/20 text-[#9b87f5]">
+                                <p className="text-[17px]">Ficção</p>
+                            </div>
+                        </div>
+                        <div className="w-full flex flex-col mt-8">
+                            <h4 className="font-bold text-[20px]">Diretor</h4>
+                            <p className="text-[#8a898c]">Christopher Edward Nolan</p>
+                        </div>
+                        <div className="w-full flex flex-col mt-8">
+                            <h4 className="font-bold text-[20px]">Sinopse</h4>
+                            <p className="text-[#8a898c]">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                 Voluptatum facere cupiditate soluta, consequuntur suscipit aspernatur dignissimos, 
+                                 repudiandae et dolores quos aut repellat quibusdam, harum exercitationem alias. 
+                                 Ipsam id odio voluptas?
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </PageWrapper>
     )
